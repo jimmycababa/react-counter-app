26a36039bb69191c966136755dc460fe886c1f85
jimmycababa <jimmycababa@gmail.com>
2021-07-01 13:34:30 -0500

    added reload button

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  
 
   const [count, setCount] = useState(0)
 
   const updateCount = () => {
     setCount(count + 1)
+
+  
   }
+
+  
+
+  const refreshPage = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <button onClick={updateCount}>Click For One More</button>
+          <button onClick={refreshPage}>Click To Reload</button>
         </p>
         <h4>You've Clicked {count} times!</h4>
           
